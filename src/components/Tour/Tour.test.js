5a77140678acaf98a93822acf66bff4fc36e9aec
Mauricio <magalocr@gmail.com>
2019-06-22 16:55:29 -0600

    Added tests to the Tour component.

--- src/components/Tour/Tour.test.js
+++ src/components/Tour/Tour.test.js
@@ -1,16 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+
 import Tour from './Tour';
 
-test('renders without crashing (smoke test)', () => {
-    const props = {
-        title: 'Skip the Line: Neues Museum Berlin Tickets',
-        price: '46',
-        currency: '$',
-        rating: '5',
-        isSpecialOffer: true
-    };
-    const div = document.createElement('div');
-    ReactDOM.render(<Tour {...props} />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Tour component', () => {
+    test('renders without crashing (smoke test)', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Tour />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    describe('displays atrtibutes properly', () => {
+        let props;
+
+        beforeEach(() => {
+            props = {
+                title: 'Skip the Line: Neues Museum Berlin Tickets',
+                price: '46',
+                currency: '$',
+                rating: '5',
+                isSpecialOffer: true
+            };
+        });
+
+        test('adds the title attribute', () => {
+            const { container } = render(<Tour {...props} />);
+            const elementWithTitle = container.querySelectorAll('div[title="Special Offer"]');
+            expect(elementWithTitle.length).toBe(1);
+        });
+
+        test('adds the title attribute', () => {
+            props.isSpecialOffer = false;
+            const { container } = render(<Tour {...props} />);
+            const elementWithTitle = container.querySelectorAll('div[title="Special Offer"]');
+            expect(elementWithTitle.length).toBe(0);
+        });
+
+        test('displays the special offer icon', () => {
+            const { container } = render(<Tour {...props} />);
+            const icon = container.querySelectorAll('.fa-tags');
+            expect(icon.length).toBe(1);
+        });
+
+        test('does not display the special offer icon', () => {
+            props.isSpecialOffer = false;
+            const { container } = render(<Tour {...props} />);
+            const icon = container.querySelectorAll('.fa-tags');
+            expect(icon.length).toBe(0);
+        });
+
+        test('displays the correct price', () => {
+            const { container } = render(<Tour {...props} />);
+            const price = container.querySelector('.price');
+            expect(price.textContent).toBe(`${props.currency + props.price}`);
+        });
+
+        test('displays the correct title', () => {
+            const { container } = render(<Tour {...props} />);
+            const title = container.querySelector('.title');
+            expect(title.textContent).toBe(props.title);
+        });
+    });
 });
